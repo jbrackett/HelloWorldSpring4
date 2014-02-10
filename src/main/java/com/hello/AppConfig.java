af94a9c47fc1492ef9ecb073884de8562047a73c
Josh Brackett <jbrackett@gmail.com>
2014-02-09 21:12:37 -0500

    Some minor cleanup

--- src/main/java/com/hello/AppConfig.java
+++ src/main/java/com/hello/AppConfig.java
@@ -2,10 +2,8 @@ package com.hello;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
-@EnableScheduling
 @ComponentScan(basePackages = { "com.hello.controller", "com.hello.config" })
 public class AppConfig {
 
--- src/main/java/com/hello/WebConfig.java
+++ src/main/java/com/hello/WebConfig.java
@@ -1,14 +1,10 @@
 package com.hello;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
 @Configuration
@@ -19,15 +15,6 @@ public class WebConfig extends WebMvcConfigurerAdapter {
   public void configureDefaultServletHandling(
       DefaultServletHandlerConfigurer configurer) {
     configurer.enable();
-  }
-
-  @Bean
-  public ViewResolver viewResolver() {
-    InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-    resolver.setViewClass(JstlView.class);
-    resolver.setSuffix(".jsp");
-
-    return resolver;
   }
 
 }
